9e7c74c1e3970ccb30bb427f19d236611eeaf4a6
Pragya <pendharkarp23@gmail.com>
2025-08-18 01:00:19 +0530

    Help is Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import { Box } from "@mui/material";
 import Navbar from "@/components/Navbar";
 import Herosection from "@/components/Herosection";
 import Companies from "@/components/Companies";
+import Help from "@/components/Help";
 function page() {
   return (
     <Box>
       <Navbar />
       <Herosection />
       <Companies />
+      <Help />
     </Box>
   );
 }
